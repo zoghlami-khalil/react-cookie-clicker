c8a45c620b98070220d4f755d99a12eb5ad0605b
Khalil Zoghlami <khalil.zoghlami@medtech.tn>
2022-01-13 14:49:09 +0100

    Implemented the automatic bonus

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,27 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 
 const Main = () => {
     const [clicks,setClicks] = useState(0);
+    const [step,setStep] = useState(1);
     const handleClick = ()=> {
-        setClicks(clicks +1);
+        setClicks(clicks +step);
     }
+
+   useEffect(() => {
+       setInterval(()=>{
+        console.log("first step",step)   
+        setStep(step*2)
+        console.log("second step",step)  
+           setTimeout(()=>setStep(step),5000)
+        console.log("third step",step)  
+       },20000)
+       
+   },[])
     
     return (
-        <div>
+        <div> 
             <h1>{`Number of clicks ${clicks}`}</h1>
+            <h1>{`Number of Steps ${step}`}</h1>
             <img onClick={()=> handleClick()} src="https://www.pngall.com/wp-content/uploads/2016/07/Cookie-Download-PNG.png" alt="Cookie" />
         </div>
     )
