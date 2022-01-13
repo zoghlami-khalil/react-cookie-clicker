4f3e12ae14d84cd1e5badb3db883fba081809419
Khalil Zoghlami <khalil.zoghlami@medtech.tn>
2022-01-13 15:21:54 +0100

    Implemented shadow clickers

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,8 +3,13 @@ import React, {useState,useEffect} from 'react'
 const Main = () => {
     const [clicks,setClicks] = useState(0);
     const [step,setStep] = useState(1);
+    const [shadowClickers,setshadowClickers] = useState(0);
     const handleClick = ()=> {
         setClicks(clicks +step);
+    }
+    const handleBuyShadowClicker = () => {
+        setClicks(prevClicks=>prevClicks -100)
+        setshadowClickers(shadowClickers + 1)
     }
 
    useEffect(() => {
@@ -15,13 +20,20 @@ const Main = () => {
            setTimeout(()=>setStep(step),5000)
         console.log("third step",step)  
        },20000)
-       
+    //eslint-disable-next-line   
    },[])
+
+   useEffect(() => {
+    clearInterval()
+    setInterval(()=>setClicks(prevClicks => prevClicks + shadowClickers ),1000)
+   },[shadowClickers])
     
     return (
         <div> 
             <h1>{`Number of clicks ${clicks}`}</h1>
             <h1>{`Number of Steps ${step}`}</h1>
+            <h1>{`Number of shadowclickers user has bought: ${shadowClickers}`}</h1>
+            <button disabled={clicks<100} onClick={()=>handleBuyShadowClicker()}>Buy shadowClicker</button>
             <img onClick={()=> handleClick()} src="https://www.pngall.com/wp-content/uploads/2016/07/Cookie-Download-PNG.png" alt="Cookie" />
         </div>
     )
